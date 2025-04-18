093e05d17e8a5e2fe2454bf8c25569c54cd190f3
VikasThakur <vikasthakur1723@gmail.com>
2025-04-18 23:19:58 +0530

    first time initialize the code

---
+++ index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+const app = express();
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+
+    
+})
